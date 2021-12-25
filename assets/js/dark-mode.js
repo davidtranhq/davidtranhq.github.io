d968508c81108f90e76076e5f33803b3cf272883
David Tran <davidtranhq@gmail.com>
2021-12-25 04:33:12 -0500

    testing dark mode

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,10 +1,12 @@
 const getTheme = () => localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 document.addEventListener("DOMContentLoaded", function () {
+  console.log("loaded");
   const toggleButton = document.querySelector('button.theme-switch');
 
   function toggleTheme(e) {
     const currentTheme = getTheme();
+    console.log(`toggling: ${currentTheme}`);
     if (!currentTheme)
       return;
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
