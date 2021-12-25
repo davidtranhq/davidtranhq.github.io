b0b450b00529e55beb0a2f205f4cb50ef0cf823c
David Tran <davidtranhq@gmail.com>
2021-12-25 04:36:54 -0500

    fixed dark mode

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,14 +1,10 @@
 const getTheme = () => localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("loaded");
   const toggleButton = document.querySelector('button.theme-switch');
 
   function toggleTheme(e) {
     const currentTheme = getTheme();
-    console.log(`toggling: ${currentTheme}`);
-    if (!currentTheme)
-      return;
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
