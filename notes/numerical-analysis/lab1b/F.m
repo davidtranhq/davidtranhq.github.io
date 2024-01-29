96ed3fa9d9aec73acbd88559472f552379d0d0a6
davidtranhq <davidtranhq@gmail.com>
2024-01-28 22:52:26 -0500

    numerical analysis lab

--- notes/numerical-analysis/lab1b/F.m
+++ notes/numerical-analysis/lab1b/F.m
@@ -1,11 +1,3 @@
-% Evaluate F(x) using the first n terms of its Taylor series.
-function T = F(x, n)
-    % Initialize sum as 0
-    T = 0;
-    % Loop over terms in series
-    for k = 0:n
-        T += (-1)^k * x.^k ./ factorial(k + 2);
-    end
+function T = F(x)
+    T = (exp(-x) - 1 + x) ./ x .^ 2;
 end
-
-% Plot F(x).
